3a03d1878527b0d3dab7f7b8ace41c502e14dc63
Jihan Abdul Rohman <jihannabdul@gmail.com>
2022-11-24 12:01:04 +0700

    first commit, start project

---
+++ src/pages/GetStarted/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "../../assets/components";
+
+function GetStarted() {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
+}
+
+export default GetStarted;
